8eb8ffcad5f509e881b797b003a82e8d63deb1cf
ahmed khaled <ahmedkhaled5562.ak@gmail.com>
2023-08-06 01:24:01 +0300

    adding project files with UI and MVC

---
+++ ByteCoine/Controller/ByteCoineViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ByteCoineViewController.swift
+//  ByteCoine
+//
+//  Created by ahmed khaled on 06/08/2023.
+//
+
+import UIKit
+
+class ByteCoineViewController: UIViewController {
+    
+    let coinManager = CoinManager()
+    
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var bitcoinLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+    }
+   
+}
+
+extension ByteCoineViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+}
+
+extension ByteCoineViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return coinManager.currencyArray[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print(row)
+    }
+}
---
+++ ByteCoine/Model/CoinManager.swift
@@ -0,0 +1,18 @@
+//
+//  CoinManager.swift
+//  ByteCoine
+//
+//  Created by ahmed khaled on 06/08/2023.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+    
+}
